5991bf1cc0d1f4cc95e211f26364d41238d177b3
jiaozhijun <jiaozhijun@hotmail.com>
2018-07-17 17:22:26 +0800

    shell reci

--- basicAlgorithm/src/main/java/utils/Utils.java
+++ basicAlgorithm/src/main/java/utils/Utils.java
@@ -4,15 +4,15 @@ import java.util.Random;
 
 public class Utils {
     public static void main(String[] args) {
-        Integer a[]=generateIntArray();
+        Integer a[]=generateIntArray(10);
         for (int i=0;i< a.length;i++) System.out.println(a[i]);
     }
-    public static  Integer [] generateIntArray(){
+    public static  Integer [] generateIntArray(int l){
         Random r=new Random();
         //int i=r.nextInt(20);
-        Integer [] a= new Integer[10];
+        Integer [] a= new Integer[l];
         for(int i=0;i<a.length;i++){
-            a[i]=r.nextInt(100);
+            a[i]=r.nextInt(100000);
         }
 
 
---
+++ basicAlgorithm/src/test/java/Circle.java
@@ -0,0 +1,7 @@
+import static java.lang.Thread.sleep;
+
+public class Circle {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread();
+    }
+}
