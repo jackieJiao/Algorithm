8048e97470bca82b65049e8f18441c409a91ac7e
jiaozhijun <jiaozhijun@hotmail.com>
2018-08-15 21:20:56 +0800

    java8

--- basicAlgorithm/src/test/java/Circle.java
+++ basicAlgorithm/src/test/java/Circle.java
@@ -2,7 +2,7 @@ import static java.lang.Thread.sleep;
 
 public class Circle {
     public static void main(String[] args) throws InterruptedException {
-        String s="部门周工作表 吕丹丹";
-        System.out.println(s.contains("吕v丹丹"));
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
     }
 }
